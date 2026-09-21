5ffb455f8dd2390356d8146da43e06408c706e5a
Garion <garionhystek@gmail.com>
2026-09-21 19:39:32 +1000

    Add folder/exe pickers, first-run settings prompt, and dark theme

--- src/ThiefManager/Views/SettingsWindow.xaml.cs
+++ src/ThiefManager/Views/SettingsWindow.xaml.cs
@@ -1,13 +1,47 @@
 using System.Windows;
+using Microsoft.Win32;
 using ThiefManager.ViewModels;
 
 namespace ThiefManager.Views;
 
 public partial class SettingsWindow : Window
 {
+    private readonly SettingsViewModel _viewModel;
+
     public SettingsWindow(SettingsViewModel viewModel)
     {
         InitializeComponent();
+        _viewModel = viewModel;
         DataContext = viewModel;
+    }
+
+    private void BrowseThief1FmFolder_Click(object sender, RoutedEventArgs e) =>
+        BrowseFolder(path => _viewModel.Thief1FmFolder = path);
+
+    private void BrowseThief2FmFolder_Click(object sender, RoutedEventArgs e) =>
+        BrowseFolder(path => _viewModel.Thief2FmFolder = path);
+
+    private void BrowseThief1ExePath_Click(object sender, RoutedEventArgs e) =>
+        BrowseExecutable(path => _viewModel.Thief1ExePath = path);
+
+    private void BrowseThief2ExePath_Click(object sender, RoutedEventArgs e) =>
+        BrowseExecutable(path => _viewModel.Thief2ExePath = path);
+
+    private void BrowseFolder(Action<string> onSelected)
+    {
+        var dialog = new OpenFolderDialog { Title = "Select Folder" };
+        if (dialog.ShowDialog(this) == true)
+            onSelected(dialog.FolderName);
+    }
+
+    private void BrowseExecutable(Action<string> onSelected)
+    {
+        var dialog = new OpenFileDialog
+        {
+            Title = "Select Executable",
+            Filter = "Executable files (*.exe)|*.exe|All files (*.*)|*.*"
+        };
+        if (dialog.ShowDialog(this) == true)
+            onSelected(dialog.FileName);
     }
 }
